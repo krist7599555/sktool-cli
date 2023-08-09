b9d168b3af1cc7b56c8368acfde1fe2373736acc
Krist Ponpairin <krist7599555@gmail.com>
2023-08-09 10:51:45 +0700

    feat: add sverlte icon

--- src/cli.mjs
+++ src/cli.mjs
@@ -14,7 +14,8 @@ const COMMANDS = /** @type {const} */ ([
   'tailwind+postcss',
   'wireify',
   'krist7599555-lodosh',
-  'icon'
+  'icon',
+  'sveltekit-svg',
 ]);
 
 /**
@@ -55,7 +56,7 @@ const { target_directory, selected_commands } = await inquirer.prompt([
     name: 'selected_commands',
     message: 'Command To run',
     choices: COMMANDS,
-    default: COMMANDS,
+    default: COMMANDS.filter(it => it != "icon"),
   },
 ]);
 
@@ -65,79 +66,110 @@ if (selected_commands.includes('wireify')) {
   await cmd_wireify(project_dir);
 }
 
-
 if (selected_commands.includes('eslint+prettier+vscode')) {
   cd(project_dir.pathname);
-  await $`pnpm i -D eslint eslint-config-prettier eslint-define-config eslint-plugin-html eslint-plugin-json-format eslint-plugin-prettier eslint-plugin-simple-import-sort eslint-plugin-svelte eslint-plugin-tailwindcss eslint-plugin-unicorn prettier-plugin-svelte prettier-plugin-tailwindcss eslint-formatter-mo`
-  console.log()
-  console.log("  > SHOULD RUN", 'eslint . --color --format mo')
-  console.log()
+  await $`pnpm i -D eslint eslint-config-prettier eslint-define-config eslint-plugin-html eslint-plugin-json-format eslint-plugin-prettier eslint-plugin-simple-import-sort eslint-plugin-svelte eslint-plugin-tailwindcss eslint-plugin-unicorn prettier-plugin-svelte prettier-plugin-tailwindcss eslint-formatter-mo`;
+  console.log();
+  console.log('  > SHOULD RUN', 'eslint . --color --format mo');
+  console.log();
   await fs.copyFile(
     new URL('./.eslintrc.cjs', ASSETS_DIR),
-    new URL('./.eslintrc.cjs', project_dir),
+    new URL('./.eslintrc.cjs', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
   await fs.copyFile(
     new URL('./.prettierrc.cjs', ASSETS_DIR),
-    new URL('./.prettierrc.cjs', project_dir),
+    new URL('./.prettierrc.cjs', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
-  fs.mkdir(new URL('./.vscode', project_dir), { recursive: true })
+  fs.mkdir(new URL('./.vscode', project_dir), { recursive: true });
   await fs.copyFile(
     new URL('./settings.json', ASSETS_DIR),
-    new URL('./.vscode/settings.json', project_dir),
+    new URL('./.vscode/settings.json', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
 }
 
 if (selected_commands.includes('tailwind+postcss')) {
   cd(project_dir.pathname);
-  await $`pnpm i -D tailwindcss postcss postcss-load-config autoprefixer @fontsource/noto-sans-thai @fontsource/noto-sans`
+  await $`pnpm i -D tailwindcss postcss postcss-load-config autoprefixer @fontsource/noto-sans-thai @fontsource/noto-sans`;
   await fs.copyFile(
     new URL('./tailwind.config.cjs', ASSETS_DIR),
-    new URL('./tailwind.config.cjs', project_dir),
+    new URL('./tailwind.config.cjs', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
   await fs.copyFile(
     new URL('./postcss.config.cjs', ASSETS_DIR),
-    new URL('./postcss.config.cjs', project_dir),
+    new URL('./postcss.config.cjs', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
   await fs.copyFile(
     new URL('./+layout.svelte', ASSETS_DIR),
-    new URL('./src/routes/+layout.svelte', project_dir),
+    new URL('./src/routes/+layout.svelte', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
   await fs.copyFile(
     new URL('./app.css', ASSETS_DIR),
-    new URL('./src/app.css', project_dir),
+    new URL('./src/app.css', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
 }
 
 if (selected_commands.includes('seo')) {
   cd(project_dir.pathname);
-  await $`pnpm i -D svelte-meta-tags`
+  await $`pnpm i -D svelte-meta-tags`;
   // await $`sed '/env/d' ./.gitignore > ./.gitignore` // un ignore .env file
-  await fs.mkdir(new URL('./src/lib', project_dir), { recursive: true })
+  await fs.mkdir(new URL('./src/lib', project_dir), { recursive: true });
   await fs.copyFile(
     new URL('./seo.svelte', ASSETS_DIR),
-    new URL('./src/lib/seo.svelte', project_dir),
+    new URL('./src/lib/seo.svelte', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
   await fs.copyFile(
     new URL('./.env', ASSETS_DIR),
-    new URL('./.env', project_dir),
+    new URL('./.env', project_dir)
     // fs.constants.COPYFILE_FICLONE_FORCE
   );
 }
 
 if (selected_commands.includes('krist7599555-lodosh')) {
   cd(project_dir.pathname);
-  await $`pnpm i -D @krist7599555/lodosh`
+  await $`pnpm i -D @krist7599555/lodosh`;
 }
 
 if (selected_commands.includes('icon')) {
   cd(project_dir.pathname);
-  await $`pnpm i -D @steeze-ui/svelte-icon @steeze-ui/lucide-icons @steeze-ui/heroicons`
+  await $`pnpm i -D @steeze-ui/svelte-icon @steeze-ui/lucide-icons @steeze-ui/heroicons`;
+}
+
+if (selected_commands.includes('sveltekit-svg')) {
+  await $`pnpm i @poppanator/sveltekit-svg -D`;
+  await fs
+    .readFile('./vite.config.ts', { encoding: 'utf-8' })
+    .then(text => `import svg from '@poppanator/sveltekit-svg';\n` + text)
+    .then(text =>
+      text.replace(
+        `sveltekit()`,
+        `sveltekit(),
+        svg({
+          includePaths: ['./src/lib/icons/', './src/assets/icons/'],
+          svgoOptions: {
+            multipass: true,
+            plugins: [
+              {
+                name: 'preset-default',
+                // by default svgo removes the viewBox which prevents svg icons from scaling
+                // not a good idea! https://github.com/svg/svgo/pull/1461
+                params: { overrides: { removeViewBox: false } },
+              },
+              { name: 'removeAttrs', params: { attrs: '(fill|stroke)' } },
+            ],
+          },
+        })`))
+    .then(text => fs.writeFile('./vite.config.ts', text));
+
+  await fs
+    .readFile('./app.d.ts', { encoding: 'utf-8' })
+    .then(text => `import '@poppanator/sveltekit-svg/dist/svg';\n` + text)
+    .then(text => fs.writeFile('./src/app.d.ts', text));
 }
